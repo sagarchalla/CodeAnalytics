73a254d6fde39b520e0e34e3c44402c36e7af20e
sagarchalla <challa492@gmail.com>
2020-09-23 11:25:25 -0600

    Changes

---
+++ src/algoritms/misc/practice/arrays/ArrayProblems.java
@@ -0,0 +1,18 @@
+package algoritms.misc.practice.arrays;
+
+public class ArrayProblems {
+
+    public static void main(String[] args) {
+        System.out.println(containsDuplicate(new int[]{0, 0, 2, 1, 1, 2, 2}));
+        System.out.println(productOfIntExceptSelf(new int[]{0, 0, 2, 1, 1, 2, 2}));
+     }
+
+    private static boolean productOfIntExceptSelf(int[] ints) {
+     return false;
+    }
+
+    private static boolean containsDuplicate(int[] nums) {
+     return false;
+    }
+
+}
---
+++ src/leetcode/problems/dynamicprogramming/DynamicProgramming.java
@@ -0,0 +1,23 @@
+package leetcode.problems.dynamicprogramming;
+
+public class DynamicProgramming {
+
+    public static void main(String[] args) {
+        climbingStairCase();
+    }
+
+    /**
+     *
+     * LeetCode Problem Number - 70
+     *
+     * You are climbing a stair case. It takes n steps to reach to the top.
+     * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+     *
+     *
+     */
+    private static void climbingStairCase() {
+
+        //
+
+    }
+}
