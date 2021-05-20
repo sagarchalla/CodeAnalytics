cdc6d3560576ab6be7ba6ff6919197b003cd33f3
135261 <sagar.challa@arrow.com>
2021-05-20 09:19:38 -0600

    Changes

--- src/grokking.java/PatternDFS.java
+++ src/grokking.java/PatternDFS.java
@@ -1,8 +1,55 @@
 package grokking.java;
 
+import cracking.coding.trees.TreeNode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 // depth first search
 public class PatternDFS {
 
+    // problem - has path
+    static boolean hasPath(TreeNode root, int number){
+        if (root == null) return false;
 
+        // recursive approach condition
+        if(root.data == number && root.left == null && root.right == null){
+            return true;
+        }
+        return hasPath(root.left , number - root.data) || hasPath(root.right , number - root.data);
+    }
+    public static List<List<Integer>> findPaths(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList();
+        if (root == null) return res;
+        dfs(root, sum, res, new ArrayList());
+        return res;
+    }
+    private static void dfs(TreeNode root, int target, List<List<Integer>> res, List<Integer> list) {
+        if (root == null) return;
+        list.add(root.data);
+        if (root.left == null && root.right == null && target == root.data)
+            res.add(new ArrayList(list));
+        dfs(root.left, target - root.data, res, list);
+        dfs(root.right, target - root.data, res, list);
+        list.remove(list.size() - 1);
+    }
+
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(4);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        int sum = 23;
+        List<List<Integer>> result = findPaths(root, sum);
+        System.out.println("Tree paths with sum " + sum + ": " + result);
+    }
 
 }
+
+
+
+
---
+++ src/grokking.java/PatternTopKElements.java
@@ -0,0 +1,43 @@
+package grokking.java;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
+public class PatternTopKElements {
+
+
+    static List<Integer> findKLargestNumbers(int[] numbers, int k) {
+
+        // solve using min heap method
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((n1,n2) -> n1 - n2);
+
+        // create min heap
+        for(int i =0; i < k; i++) {
+            priorityQueue.add(numbers[i]);
+        }
+
+        // make the heap with the three largest numbers
+        for(int j = k ; j < numbers.length; j++) {
+              if(numbers[j] > priorityQueue.peek()) {
+                priorityQueue.poll();
+                priorityQueue.add(numbers[j]);
+              }
+
+        }
+
+   return new ArrayList<>(priorityQueue);
+
+    }
+
+
+    public static void main(String[] args) {
+        List<Integer> result = findKLargestNumbers(new int[] { 3, 1, 5, 12, 2, 11 }, 3);
+        System.out.println("Here are the top K numbers: " + result);
+
+        result = findKLargestNumbers(new int[] { 5, 12, 11, -1, 12 }, 3);
+        System.out.println("Here are the top K numbers: " + result);
+    }
+}
+
